9df12d0629d22fcea14b5306092f4d649711a833
Julian Coleman <julcol03@gmail.com>
2017-11-24 22:18:55 -0800

    add first batch of functions

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const assocByKey = require("./lib/assoc-by-key");
+const coerceBoolean = require("./lib/coerce-boolean");
+const indexByKey = require("./lib/index-by-key");
 
+module.exports = {
+  assocByKey,
+  coerceBoolean,
+  indexByKey,
+};
