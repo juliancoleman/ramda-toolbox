e81d89d2a79fdc99181ffd3f15fd27578fe8f2f3
Julian Coleman <julcol03@gmail.com>
2017-11-25 02:11:56 -0800

    add more functions!!!!

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const assocByKey = require("./lib/assoc-by-key");
+const bitwiseAnd = require("./lib/bitwise-and");
+const bitwiseLeftShift = require("./lib/bitwise-left-shift");
+const bitwiseNot = require("./lib/bitwise-not");
+const bitwiseOr = require("./lib/bitwise-or");
+const bitwiseSPRightShift = require("./lib/bitwise-right-shift");
+const bitwiseXor = require("./lib/bitwise-xor");
+const bitwiseZFRightShift = require("./lib/bitwise-zero-fill-right-shift");
+const btoi = require("./lib/map-bool-to-int");
 const coerceBoolean = require("./lib/coerce-boolean");
+const filterInt = require("./lib/filter-int");
+const fullJoin = require("./lib/full-join");
 const indexByKey = require("./lib/index-by-key");
+const innerJoin = require("./lib/inner-join");
+const leftJoin = require("./lib/left-join");
+const mask = require("./lib/mask");
+const rightJoin = require("./lib/right-join");
 
 module.exports = {
   assocByKey,
+  bitwiseAnd,
+  bitwiseLeftShift,
+  bitwiseNot,
+  bitwiseOr,
+  bitwiseSPRightShift,
+  bitwiseXor,
+  bitwiseZFRightShift,
+  btoi,
   coerceBoolean,
+  filterInt,
+  fullJoin,
   indexByKey,
+  innerJoin,
+  leftJoin,
+  mask,
+  rightJoin,
 };
